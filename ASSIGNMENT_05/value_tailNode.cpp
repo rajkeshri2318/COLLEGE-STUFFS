dd56096622cd71e32c8cf23903edac1e4b2dd022
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2023-02-12 16:18:43 +0530

    Create value_tailNode.cpp

---
+++ ASSIGNMENT_05/value_tailNode.cpp
@@ -0,0 +1,14 @@
+SinglyLinkedListNode* temp=llist;
+    int count=0;
+    
+    while(temp!=NULL){
+        temp=temp->next;
+        count++;
+    }
+    int po=count-positionFromTail-1;
+    int i=0;
+    while(po>i){
+        llist=llist->next;
+        i++;
+    }
+    return llist->data;
